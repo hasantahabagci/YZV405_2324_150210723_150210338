5947ff981d11051ec230da921b693f15a8956d58
Hasan Taha Bagci <bagcih21@itu.edu.tr>
2024-04-28 12:53:06 +0300

    Loss function added.

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,9 @@
-import torch.nn.functional as F
+import torch
+import torch.nn as nn
 
 
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+def cross_entropy_loss(logits, targets):
+    log_probs = nn.functional.log_softmax(logits, dim=-1)
+    target_log_probs = log_probs.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze(-1)
+    loss = -target_log_probs.mean()
+    return loss
