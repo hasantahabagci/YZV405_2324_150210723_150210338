95761747cb1209976860393a9025dad7a67e14b3
Hasan Taha Bagci <bagcih21@itu.edu.tr>
2024-04-28 13:12:29 +0300

    Add Seq2Seq model for sequence-to-sequence tasks

--- model/model.py
+++ model/model.py
@@ -1,22 +1,88 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from base import BaseModel
+import torch.optim as optim
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import random
 
+class Encoder(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, dropout):
+        super(Encoder, self).__init__()
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=dropout, bidirectional=True)
+        self.dropout = nn.Dropout(dropout)
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+    def forward(self, input_seq, input_lengths):
+        embedded = self.dropout(self.embedding(input_seq))
+        packed_embedded = pack_padded_sequence(embedded, input_lengths, enforce_sorted=False)
+        packed_outputs, (hidden, cell) = self.rnn(packed_embedded)
+        outputs, _ = pad_packed_sequence(packed_outputs)
+        return outputs, hidden, cell
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+class Attention(nn.Module):
+    def __init__(self, hidden_size):
+        super(Attention, self).__init__()
+        self.attn = nn.Linear(hidden_size * 3, hidden_size)
+        self.v = nn.Linear(hidden_size, 1, bias=False)
+
+    def forward(self, hidden, encoder_outputs, mask):
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+        attention = self.v(masked_fill(mask == 0, -1e10))
+        return torch.softmax(attention.squeeze(2), dim=1)
+
+class Decoder(nn.Module):
+    def __init__(self, output_size, embedding_size, hidden_size, num_layers, dropout, attention):
+        super(Decoder, self).__init__()
+        self.output_size = output_size
+        self.attention = attention
+        self.embedding = nn.Embedding(output_size, embedding_size)
+        self.rnn = nn.LSTM(hidden_size * 2 + embedding_size, hidden_size, num_layers, dropout=dropout)
+        self.fc_out = nn.Linear(hidden_size * 3, output_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, cell, encoder_outputs, mask):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        attn_weights = self.attention(hidden[-1], encoder_outputs, mask)
+        attn_weights = attn_weights.unsqueeze(1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        weighted = torch.bmm(attn_weights, encoder_outputs)
+        weighted = weighted.permute(1, 0, 2)
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        output, (hidden, cell) = self.rnn(rnn_input, (hidden, cell))
+        assert (output == hidden).all()
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim=1))
+        return prediction, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, src_pad_idx, device):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_pad_idx = src_pad_idx
+        self.device = device
+
+    def create_mask(self, src):
+        mask = (src != self.src_pad_idx).permute(1, 0)
+        return mask
+
+    def forward(self, src, src_len, trg, teacher_forcing_ratio=0.5):
+        trg_len = trg.shape[0]
+        batch_size = trg.shape[1]
+        trg_vocab_size = self.decoder.output_size
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+        encoder_outputs, hidden, cell = self.encoder(src, src_len)
+        input = trg[0, :]
+        mask = self.create_mask(src)
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(input, hidden, cell, encoder_outputs, mask)
+            outputs[t] = output
+            best_guess = output.argmax(1)
+            input = trg[t] if random.random() < teacher_forcing_ratio else best_guess
+        return outputs
